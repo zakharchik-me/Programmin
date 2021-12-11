9054732906d8bd9f5b98d4b6023a6e39e2ca39cb
Danniel Balint <89006395513.d@gmail.com>
2021-12-11 21:00:27 +0300

    L2

---
+++ LAb2_2/cmake-build-debug/CMakeFiles/LAb2_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAb2_2.dir/main.c.obj"
+  "LAb2_2.exe"
+  "LAb2_2.exe.manifest"
+  "LAb2_2.pdb"
+  "libLAb2_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAb2_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++
@@ -1,2 +0,0 @@
-Tut nichego netu
-
