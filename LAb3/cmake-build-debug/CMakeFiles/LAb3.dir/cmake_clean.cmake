b30dc9585064c3443eaf8656b49d0cd621c2a4ec
Danniel Balint <89006395513.d@gmail.com>
2021-12-11 21:07:56 +0300

    L3

---
+++ LAb3/cmake-build-debug/CMakeFiles/LAb3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAb3.dir/main.cpp.obj"
+  "LAb3.exe"
+  "LAb3.exe.manifest"
+  "LAb3.pdb"
+  "libLAb3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAb3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
